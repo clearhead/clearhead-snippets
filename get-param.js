54e5a0223d99e12c5d58782d802c4ad5b5c80927
Tom Fuertes <tomfuertes@gmail.com>
2014-06-25 15:17:14 -0500

    tabs -> spaces & linting
    for @beaktor w/ the hopes he supports the mochimobile
    
    * jshintrc from airbnb
    * editorconfig from chronos

--- get-param.js
+++ get-param.js
@@ -2,15 +2,17 @@
 /**
  * Get querystring param by name
  * ```
- * 	  window.location.search; // ?foo=bar
+ *    window.location.search; // ?foo=bar
  *    clearhead.getParam('foo'); // bar
  * ```
  */
 window.clearhead = window.clearhead || {};
 window.clearhead.getParam = function (name) {
-	name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-	var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
-		results = regex.exec(location.search);
-	return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  'use strict';
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
+    results = regex.exec(location.search);
+  return results === null ?
+    '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
 /* _optimizely_evaluate=safe */
